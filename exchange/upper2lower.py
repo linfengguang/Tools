7c577feec3303bd9cf6f43eaa69e80b3a485854d
linfengguang <453093194@qq.com>
2017-11-09 13:23:06 +0800

    commit

---
+++ exchange/upper2lower.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+class ExchangeTools(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.creatWidgets()
+
+    def creatWidgets(self):
+        self.contentInput = Entry(self)
+        self.contentInput.pack()
+        self.alertButton = Button(self, text='大写转换', width=50, height=20, command=self.upper2lower)
+        self.alertButton.pack()
+
+    def upper2lower(self):
+        content = self.contentInput.get()
+        messagebox.showinfo('Message', '%s' % str(content).upper())
+
+if __name__ == "__main__":
+    app = ExchangeTools()
+    app.master.title('ExchangeTools')
+    app.mainloop()
